75a6c16cff6e72ab1bf6e99906048366765c6bf5
Doziechuks <doziechuks1010@gmail.com>
2023-09-07 11:07:08 +0100

    stars upated

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -15,7 +15,18 @@ function StarRating({ rating }) {
     If the rating is 4, we need 4 copies.
   */
 
-  return <div className="star-wrapper"></div>;
+  return <div className="star-wrapper">
+    {
+      [...Array(rating)].map((_, index) => (
+        <img
+        key={index}
+        alt=""
+        className="gold-star"
+        src="https://sandpack-bundler.vercel.app/img/gold-star.svg"
+      />
+      ))
+    }
+  </div>;
 }
 
 export default StarRating;
